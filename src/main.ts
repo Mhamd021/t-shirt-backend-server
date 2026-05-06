2f8a354f6fc833530d913b3a27ceff72f62a2b37
Mhamd021 <mhamdghanoumstr@gmail.com>
2026-05-06 17:06:26 +0300

    production setup

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+     app.enableCors({
+    origin: process.env.FRONTEND_URL || '*',
+  });
+
   app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 3001);
 }
